0902d3e0594c303c98fdbf6a9ed6078175d495ad
gonglihai <glhly@live.com>
2025-02-07 14:24:54 +0800

    安装

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="height: 100%; width: 100%">
+    <VPage :page="pageOption"></VPage>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      pageOption: {
+        tree: {
+          field: 'deptId',
+          api: '/mook/tree.json',
+        },
+        search: [
+          { label: '账号', field: 'id' },
+          { label: '姓名', field: 'name' },
+          {
+            label: '性别', field: 'gender', type: 'select', option: [
+              { id: '0', name: '男' },
+              { id: '1', name: '女' },
+              { id: '2', name: '未知' }
+            ]
+          }
+        ],
+        button: [
+          { name: '添加', click: () => console.log('点击了添加') },
+          { name: '编辑', selectCount: 1, click: this.edit },
+          { name: '删除', selectMin: 1, click: this.del },
+        ],
+        table: {
+          select: true,
+          api: '/mook/table.json',
+          col: [
+            { field: 'id', name: '账号' },
+            { field: "name", name: "姓名" },
+            { field: "gender", name: "性别" },
+            { field: "birthday", name: "生日" },
+            { field: "age", name: "年龄" },
+            {
+              name: '联系方式', children: [
+                { field: "email", name: "电子邮件" },
+                { field: "phone", name: "电话号码" },
+              ]
+            },
+            { field: "city", name: "城市" },
+            { field: "address", name: "地址" },
+            { field: "occupation", name: "职业" },
+            { field: "company", name: "公司" },
+            { field: "hobbies", name: "爱好", fmt: (value) => value.join(',') },
+            {
+              name: '操作', type: "button", button: [
+                { name: '详情' },
+                { name: '编辑', click: ({ row }) => this.edit([row]) },
+                { name: '删除', confirmClick: ({ row }) => this.del([row]) },
+              ]
+            }
+          ]
+        }
+      }
+    }
+  },
+  methods: {
+    edit(rows) {
+      console.log('点击了编辑，选中的行：', rows)
+    },
+    del(rows) {
+      console.log('点击了删除，选中的行：', rows)
+    },
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+import VPage from 'page-element2';
+Vue.use(VPage);
+
 Vue.config.productionTip = false
 
 new Vue({
